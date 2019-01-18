6a74ae00663305b62bb5ce43516163555a404953
Ratnam Yadav <ratnam.yadav@icloud.com>
2019-01-18 16:42:41 +0530

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Ratnam Yadav on 18/01/19.
+//  Copyright © 2019 Ratnam Yadav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
